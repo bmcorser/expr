d66e02cbf5979f00aafe63819107339563a3e559
bmcorser <benmarshallcorser@gmail.com>
2014-09-28 22:43:10 +0100

    furst

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 import re
 import os
 import sys
-import subprocess
 
 from setuptools import setup
 from setuptools.command.test import test
-from setuptools.command.sdist import sdist
-
-import pandoc
-pandoc.core.PANDOC_PATH = subprocess.check_output(['which', 'pandoc']).strip()
 
 
 class pytest_(test):
@@ -29,24 +24,12 @@ class pytest_(test):
         sys.exit(errno)
 
 
-class pandoc_sdist(sdist):
-
-    def run(self):
-        sdist.run(self)  # note this isn't `super` :(
-
-
 def dunder(name):
     path_components = os.path.dirname(__file__), 'expr_graph', '__init__.py'
     with open(os.path.join(*path_components)) as initpy:
         return (re.compile(r".*{0} = '(.*?)'".format(name), re.S)
                   .match(initpy.read()).group(1))
 
-doc = pandoc.Document()
-with open('README.md') as README_md:
-    doc.markdown = README_md.read()
-with open('README.rst', 'w+') as README_rst:
-    README_rst.write(doc.rst)
-print('Created README.rst')
 setup_kwargs = {
     'author': 'bmcorser',
     'author_email': 'bmcorser@gmail.com',
@@ -59,9 +42,6 @@ setup_kwargs = {
     'long_description': open('README.rst', 'r').read(),
     'cmdclass': {
         'test': pytest_,
-        'sdist': pandoc_sdist,
     },
 }
 setup(**setup_kwargs)
-os.unlink(README_rst.name)
-print('Deleted README.rst')
